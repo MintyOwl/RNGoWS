04aced289aba5148b44ab0d984a35e54891cb7e2
Minty <mintyowl@mail.com>
2017-04-06 10:14:46 +0530

    somehow ReactMethod was not included

--- android/app/src/main/java/com/rngows/GoModule.java
+++ android/app/src/main/java/com/rngows/GoModule.java
@@ -4,6 +4,7 @@ import android.util.Log;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 
